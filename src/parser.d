a2d8c6b22cf5b07cdd53af9282c3d1b7c8cd57f6
k-langer <klanger@ccs.neu.edu>
2013-07-23 22:06:10 -0700

    Sync split

--- src/parser.d
+++ src/parser.d
@@ -1,4 +1,6 @@
-import std.stdio, std.regex, std.container;
+import std.stdio, std.regex;
+import design; 
+
 
 void parseFVile(string filename);
 
@@ -6,136 +8,12 @@ void parseFVile(string filename);
 
 
 void main(string argv[]){
+  
     Design d = parseVFile(argv[1]);
     d.print(d.getNets());
 }
 
-alias RedBlackTree !(net) nets;
-alias RedBlackTree !(port) ports;
-alias RedBlackTree !(instance) instances;
-alias RedBlackTree !(pin) pins;
-net * netp;
-port* potp;
-instance* instancep;
-pin* pinp;
 
-
-class Design {
-    nets n;
-    ports p;
-    instances i;
-    string name;
-    this (string namex)
-	{
-        name = namex;
-        n = new nets();
-        p = new ports();
-        i = new instances();
-	}
-    public void addNet(string net_s) {
-        n.insert(new net(net_s));
-    }
-    public void addPort(string port_s) {
-        p.insert(new port(port_s));
-    }
-    public void addInstance(string instance_s) {
-        i.insert(new instance(instance_s));
-    }
-    public instances getInstances() {
-        return i;
-    }
-    public nets getNets() {
-        return n;
-    }
-    public ports getPorts() {
-        return p;
-    }
-    public void print(T)(T t) {
-        foreach (n ; t) {
-            writeln(n);
-        }
-    }
-    override int opCmp (Object o)
-	{
-        if  (auto other = cast(Design) o) {    
-            return (name > other.name) - (name < other.name);
-        }
-		throw new Exception("Cannot compare non-designs");
-	}
-}
-class pin {
-    string name;
-    //auto netsOfPin = new SList();
-
-    this(string namex) 
-    {
-        name = namex;
-    }
-    override int opCmp (Object o)
-	{
-        if  (auto other = cast(pin) o) {    
-            return (name > other.name) - (name < other.name);
-        }
-		throw new Exception("Cannot compare non-designs");
-	}
-}
-
-class net {
-    string name;
-    //auto instancesOfNet = new SList();
-    
-    this(string namex) 
-    {
-        name = namex;
-    }
-    override int opCmp (Object o)
-	{
-        if  (auto other = cast(net) o) {    
-            return (name > other.name) - (name < other.name);
-        }
-		throw new Exception("Cannot compare non-designs");
-	}
-    override string toString() {
-        return name;
-    }
-}
-class port {
-    string name;
-    //auto netsOfPort  = new SList();
-    
-    this(string namex) 
-    {
-        name = namex;
-    }
-    override int opCmp (Object o)
-	{
-        if  (auto other = cast(port) o) {    
-            return (name > other.name) - (name < other.name);
-        }
-		throw new Exception("Cannot compare non-designs");
-	}
-    override string toString() {
-        return name;
-    }
-}
-class instance {
-    string name;
-
-    this(string namex) 
-    {
-        name = namex;
-    }
-    override int opCmp (Object o)
-	{
-        if  (auto other = cast(instance) o) {    
-            return (name > other.name) - (name < other.name);
-        }
-		throw new Exception("Cannot compare non-designs");
-	}
-    override string toString() {
-        return name;
-    }
-}
 
 
 Design parseVFile(string filename) {
@@ -145,7 +23,7 @@ Design parseVFile(string filename) {
 	foreach(str; File(filename).byLine()) {
 		lines ~= str.idup;
 	}
-
+    
 	//auto EOF_reg = regex(r";");
 	
 	int i = 0;
