baac35d14659d1df2c391a3387dccb124424e09e
k-langer <klanger@ccs.neu.edu>
2013-07-23 21:55:36 -0700

    Improve database; need intra-reference and expanded defs

--- src/parser.d
+++ src/parser.d
@@ -6,28 +6,55 @@ void parseFVile(string filename);
 
 
 void main(string argv[]){
-    parseVFile(argv[1]);
+    Design d = parseVFile(argv[1]);
+    d.print(d.getNets());
 }
 
+alias RedBlackTree !(net) nets;
+alias RedBlackTree !(port) ports;
+alias RedBlackTree !(instance) instances;
+alias RedBlackTree !(pin) pins;
+net * netp;
+port* potp;
+instance* instancep;
+pin* pinp;
 
 
 class Design {
-
-    alias RedBlackTree !(net) nets;
-    alias RedBlackTree !(port) ports;
-    alias RedBlackTree !(instance) instances;
-
     nets n;
     ports p;
     instances i;
     string name;
     this (string namex)
 	{
-		n = new nets();
+        name = namex;
+        n = new nets();
         p = new ports();
         i = new instances();
-        name = namex;
 	}
+    public void addNet(string net_s) {
+        n.insert(new net(net_s));
+    }
+    public void addPort(string port_s) {
+        p.insert(new port(port_s));
+    }
+    public void addInstance(string instance_s) {
+        i.insert(new instance(instance_s));
+    }
+    public instances getInstances() {
+        return i;
+    }
+    public nets getNets() {
+        return n;
+    }
+    public ports getPorts() {
+        return p;
+    }
+    public void print(T)(T t) {
+        foreach (n ; t) {
+            writeln(n);
+        }
+    }
     override int opCmp (Object o)
 	{
         if  (auto other = cast(Design) o) {    
@@ -36,46 +63,84 @@ class Design {
 		throw new Exception("Cannot compare non-designs");
 	}
 }
-
-struct net {
+class pin {
     string name;
+    //auto netsOfPin = new SList();
+
+    this(string namex) 
+    {
+        name = namex;
+    }
+    override int opCmp (Object o)
+	{
+        if  (auto other = cast(pin) o) {    
+            return (name > other.name) - (name < other.name);
+        }
+		throw new Exception("Cannot compare non-designs");
+	}
+}
+
+class net {
+    string name;
+    //auto instancesOfNet = new SList();
     
     this(string namex) 
     {
         name = namex;
     }
-    int opCmp (ref net other)
+    override int opCmp (Object o)
 	{
-		return (name > other.name) - (name < other.name);
+        if  (auto other = cast(net) o) {    
+            return (name > other.name) - (name < other.name);
+        }
+		throw new Exception("Cannot compare non-designs");
 	}
+    override string toString() {
+        return name;
+    }
 }
-struct port {
+class port {
     string name;
+    //auto netsOfPort  = new SList();
     
     this(string namex) 
     {
         name = namex;
     }
-    int opCmp (ref port other)
+    override int opCmp (Object o)
 	{
-		return (name > other.name) - (name < other.name);
+        if  (auto other = cast(port) o) {    
+            return (name > other.name) - (name < other.name);
+        }
+		throw new Exception("Cannot compare non-designs");
 	}
+    override string toString() {
+        return name;
+    }
 }
-struct instance {
+class instance {
     string name;
-    
+
     this(string namex) 
     {
         name = namex;
     }
-    int opCmp (ref instance other)
+    override int opCmp (Object o)
 	{
-		return (name > other.name) - (name < other.name);
+        if  (auto other = cast(instance) o) {    
+            return (name > other.name) - (name < other.name);
+        }
+		throw new Exception("Cannot compare non-designs");
 	}
+    override string toString() {
+        return name;
+    }
 }
 
-void parseVFile(string filename) {
 
+Design parseVFile(string filename) {
+
+    Design d;
 	string[] lines;
 	foreach(str; File(filename).byLine()) {
 		lines ~= str.idup;
@@ -89,7 +154,7 @@ void parseVFile(string filename) {
     string select;
     bool isModule = false;
     bool captured = false;
-
+    
     //line by line structural verilog parser
 	foreach(string l; lines) {
 
@@ -100,6 +165,7 @@ void parseVFile(string filename) {
             //START MODULE
             else if (auto a = match(l,r"module\s(\w*)\s")) {
                 heir = a.captures[1];
+                d = new Design(heir);
                 if (match(l,r";")){isModule = false;}else{isModule = true;}
                 continue;
             } else if (isModule) {
@@ -113,7 +179,7 @@ void parseVFile(string filename) {
                 }
             //PORT
                 if (select == "input" || select == "output" ) {
-                    writeln("port");
+                    //writeln("port");
                     bool endBus;
                     int width = 1;
                     foreach(m ; match(l,regex(r"([\w:]*)[,\];]","gm"))) {
@@ -130,31 +196,35 @@ void parseVFile(string filename) {
                             endBus = true;
                         } 
                         if (endBus) {
-                            writeln("\t" , m.captures[1] , " " , width);
+                            //writeln("\t" , m.captures[1] , " " , width);
+                            d.addPort(m.captures[1]);
                         }
                             
                     }
             //NET
                 } else if ( select == "wire" ) {
-                    writeln("net"); 
+                    //writeln("net"); 
                     foreach (pinNet; match(l,regex(r"(\w*),","gm"))){
-                        writeln("\t" , pinNet.captures[1]);
+                        //writeln("\t" , pinNet.captures[1]);
+                        d.addNet(pinNet.captures[1]);
                     }
 
                 } 
             //INSTANCE            
                 else {
                     if (!captured) {
-                        writeln(select);
+                        //writeln(select);
                         captured = true;
                         //Get instance name, broken if on seperate line from cell name
                         if ( auto name_regx = match(l,r"\b\w*\b\s\\?([\w\[\]\d]*)") ) {
-                            writeln(name_regx.captures[1]);
+                            //writeln(name_regx.captures[1]);
+                            d.addInstance( name_regx.captures[1]);
                         }
                     }
                     //Get all pins and attached nets
                     foreach (pinNet; match(l,regex(r"\.(\w*)\(([\w\[\]\d]*)\)","gm"))){
-                        writeln("\t" , pinNet.captures[1] , " " , pinNet.captures[2] );
+                        //writeln("\t" , pinNet.captures[1] , " " , pinNet.captures[2] );
+                        
                     }
                     
                 }
@@ -169,6 +239,7 @@ void parseVFile(string filename) {
             }
            	
 	}
+    return d;
     
 }
 
